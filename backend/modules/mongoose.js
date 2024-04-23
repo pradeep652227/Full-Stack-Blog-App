b9f71ccd8aee8200a9046699a465693db79092a4
Pradeep Gaur <ps652227@gmail.com>
2024-04-23 22:57:08 +0530

    vercel env

--- backend/modules/mongoose.js
+++ backend/modules/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config({path:"../.env"});
+// import dotenv from "dotenv";
+// dotenv.config({path:"../.env"});
 
 /*setting mongoose and database*/
 const blogPostSchema=new mongoose.Schema({

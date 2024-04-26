5a39a4c5cf5fe8368ba7431dc14bdab7099a6a8b
Pradeep Gaur <ps652227@gmail.com>
2024-04-26 19:45:14 +0530

    env files move

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig ({
-  envDir:"../",
   server:{
     proxy:{
       '/post-signup':'http://localhost:3000',

d178e98378eb4f81059901921119634433f91cec
Pradeep Gaur <ps652227@gmail.com>
2024-04-27 09:42:45 +0530

    vercel-3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,21 +5,24 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig ({
   server:{
-    proxy:{
-      '/post-signup':'http://localhost:3000',
-      '/post-login':'http://localhost:3000',
-      '/server-create-post':'http://localhost:3000',
-      '/update-post':'http://localhost:3000',
-      '/api/posts/':'http://localhost:3000',
-      '/api/delete-post/':'http://localhost:3000',
-      '/api/delete-image/':'http://localhost:3000'
-    }
     // proxy:{
-    //   '/post-signup':'https://blog-app-backend-tau.vercel.app',
-    //   '/post-login':'https://blog-app-backend-tau.vercel.app',
-    //   '/create-post-server':'https://blog-app-backend-tau.vercel.app',
-    //   '/api/public-posts':'https://blog-app-backend-tau.vercel.app'
+    //   '/post-signup':'http://localhost:3000',
+    //   '/post-login':'http://localhost:3000',
+    //   '/server-create-post':'http://localhost:3000',
+    //   '/update-post':'http://localhost:3000',
+    //   '/api/posts/':'http://localhost:3000',
+    //   '/api/delete-post/':'http://localhost:3000',
+    //   '/api/delete-image/':'http://localhost:3000'
     // }
+    proxy:{
+      '/post-signup':'https://blog-app-backend-navy-beta.vercel.app',
+      '/post-login':'https://blog-app-backend-navy-beta.vercel.app',
+      '/api/posts/':'https://blog-app-backend-navy-beta.vercel.app',
+      '/server-create-post':'https://blog-app-backend-navy-beta.vercel.app',
+      '/update-post':'https://blog-app-backend-navy-beta.vercel.app',
+      '/api/delete-post/':'https://blog-app-backend-navy-beta.vercel.app',
+      '/api/delete-image/':'https://blog-app-backend-navy-beta.vercel.app',
+    }
   }
 })
 // vite.config.js
